ec668fc6b96c4ddf297c3ad41b33b07b5aa0f3fc
Eric de Haan <E.de.Haan@chipsoft.com>
2025-12-03 10:03:28 +0100

    Added Day1, puzzle 1

---
+++ AOC.Day03/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AOC.Day03
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var x2 = Puzzle1.Calc();
+            Console.WriteLine(x2);
+
+            Console.WriteLine("Hello, World!");
+        }
+
+
+    }
+}
