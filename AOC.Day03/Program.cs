16ee38061190340c8908799dc6ec4b314f6c97e5
Eric <git@asyncvoid.com>
2025-12-04 09:52:29 +0100

    Added time benchmark

--- AOC.Day03/Program.cs
+++ AOC.Day03/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AOC.Common;
+using System.Diagnostics;
 
 namespace AOC.Day03
 {
@@ -8,11 +9,16 @@ namespace AOC.Day03
         {
             List<string> batteryBanks = LoadInput(Console.In);
 
+            var ts1 = Stopwatch.GetTimestamp();
             var puzzle1 = PuzzleRefactor.Calc(batteryBanks, 2);
             Console.WriteLine($"Puzzle 1: {puzzle1}");
 
             var puzzle2 = PuzzleRefactor.Calc(batteryBanks, 12);
             Console.WriteLine($"Puzzle 2: {puzzle2}");
+
+            var ts2 = Stopwatch.GetTimestamp();
+            Console.WriteLine("Elapsed time: " +
+                $"{(ts2 - ts1) * 1000 / (double)Stopwatch.Frequency:N0} ms");
         }
 
         private static List<string> LoadInput(this TextReader text) =>
